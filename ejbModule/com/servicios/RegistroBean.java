eb0cb4fe3699fa60245b56ac2a86e6e2ee91c480
Karen <66844562+Karen-et@users.noreply.github.com>
2021-11-07 18:06:37 -0300

    actualizaciones

---
+++ ejbModule/com/servicios/RegistroBean.java
@@ -0,0 +1,18 @@
+package com.servicios;
+
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class RegistroBean
+ */
+@Stateless
+public class RegistroBean implements RegistroBeanRemote {
+
+    /**
+     * Default constructor. 
+     */
+    public RegistroBean() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
