b01634b54e57ab28964a4d1adb16b56eeb4c6b1d
Karen <66844562+Karen-et@users.noreply.github.com>
2021-11-10 21:32:55 -0300

    Revert "Revert "08_11""
    This reverts commit e69527338326a830846c2bd0b0a6894aa0db717f.

--- ejbModule/com/servicios/CasillaBeanRemote.java
+++ ejbModule/com/servicios/CasillaBeanRemote.java
@@ -5,12 +5,14 @@ import java.util.List;
 import javax.ejb.Remote;
 
 import com.entities.Casilla;
+import com.exception.ServiciosException;
 
 @Remote
 public interface CasillaBeanRemote {
 
 	List<Casilla> obtenerTodos();
-
 	Casilla buscar(String nombre);
+	void actualizar(Casilla casilla);
+	Casilla crear(Casilla casilla) throws ServiciosException;
 
 }
